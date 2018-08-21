6e05f8f06d469dc1a6b381b7094d057d8c09df06
Matt <mintzer.chemistry@gmail.com>
2018-08-21 14:36:35 -0400

    Work on sequelize models

---
+++ server/app.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path') // may need this later
+
+const routes = require('./routes') // TODO need to set this path up
+
+// copied the below line.  Maybe need explained
+app.use(morgan('      ↓ received :method :url · responded :status :res[Content-Type]'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/', routes)
+
+// custom error handling
+app.use((err, req, res, next) => {
+    // just in case
+  if (!err.stack || !err.message) next(err)
+    // clean up the trace to just relevant info
+  const cleanTrace = err.stack
+        .split('\n')
+        .filter(line => {
+            // comment out the next two lines for full (verbose) stack traces
+          const projectFile = line.indexOf(__dirname) > -1 // omit built-in Node code
+          const nodeModule = line.indexOf('node_modules') > -1 // omit npm modules
+          return projectFile && !nodeModule
+        })
+        .join('\n')
+    // colorize and format the output
+  console.log(chalk.magenta('      ' + err.message))
+  console.log('    ' + chalk.gray(cleanTrace))
+    // send back error status
+  res.status(err.status || 500).end()
+})
+
+module.exports = app
--- server/db/index.js
+++ server/db/index.js
@@ -6,7 +6,29 @@ const Student = require('./Student')
 const Teacher = require('./Teacher')
 
 // associations VVV
+Teacher.hasMany(Post)
+Post.belongsTo(Teacher)
 
+Teacher.belongsToMany(Student, { through: 'roster' })
+Student.belongsToMany(Teacher, { through: 'roster' })
+
+Teacher.hasMany(Class)
+Class.belongsTo(Teacher)
+
+Teacher.hasMany(Comment)
+Comment.belongsTo(Teacher)
+
+Student.hasMany(Post)
+Post.belongsTo(Student)
+
+Student.belongsToMany(Class, { through: 'classroom' })
+Class.belongsToMany(Student, { through: 'classroom' })
+
+Post.belongsTo(Class)
+Class.hasMany(Post)
+
+Student.hasMany(Comment)
+Comment.belongsTo(Student)
 // associations ^^^
 
 module.exports = {
